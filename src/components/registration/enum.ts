a529a13af8fcca008e7a2136678be88ae79253d9
KarolDawidG <karoldawidg@gmail.com>
2023-11-28 20:31:07 +0100

    small refactor - karoldawidg

---
+++ src/components/registration/enum.ts
@@ -0,0 +1,14 @@
+export const expectedTypeWorkEnum = [
+    "Brak preferencji",
+    "Na miejscu",
+    "Gotowość do przeprowadzki",
+    "Wyłącznie zdalnie",
+    "Hybrydowo",
+]
+
+export const expectedContractTypeEnum = [
+    "Brak preferencji",
+    "Tylko UoP",
+    "Możliwe B2B",
+    "Możliwe UZ/UoD",
+]
