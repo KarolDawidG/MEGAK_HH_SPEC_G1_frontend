757d094ff3e056a6a565c76d1c6cc75ed2111ea2
Swichu553 <swicha@o2.pl>
2023-11-30 21:54:21 +0100

    add menuStudents

--- src/components/students/menuStudents.tsx
+++ src/components/students/menuStudents.tsx
@@ -1,16 +1,14 @@
+import React, { useState } from 'react';
 import Container from '@mui/material/Container';
 import CssBaseline from '@mui/material/CssBaseline';
 import Box from '@mui/material/Box';
-import { Typography } from '@mui/material';
-import { useState } from 'react';
-
-
+import Typography from '@mui/material/Typography';
 
 export const MenuStudents = () => {
-    const [user, setUser] = useState<string>('Marcin R');
+    const [selectedOption, setSelectedOption] = useState<'students' | 'conversation'>('students');
 
     return (
-        <Container component="main" maxWidth='xl'>
+        <Container component="main" maxWidth="xl">
             <CssBaseline />
             <Box
                 display="flex"
@@ -20,15 +18,35 @@ export const MenuStudents = () => {
                 paddingY={2}
             >
                 <Box display="flex" alignItems="center">
-                    <Typography variant="h6" component="div" margin={2} >
+                    <Typography
+                        variant="h6"
+                        component="div"
+                        margin={2}
+                        style={{
+                            cursor: 'pointer',
+                            borderBottom: selectedOption === 'students' ? '2px solid red' : 'none',
+                            padding: '0 0 8px',
+                            color: selectedOption === 'students' ? 'white' : '#d2c1b3',
+                        }}
+                        onClick={() => setSelectedOption('students')}
+                    >
                         Dostępni kursanci
                     </Typography>
-                    <Typography variant="h6" component="div" margin={2} >
+                    <Typography
+                        variant="h6"
+                        component="div"
+                        margin={2}
+                        style={{
+                            cursor: 'pointer',
+                            borderBottom: selectedOption === 'conversation' ? '2px solid red' : 'none',
+                            padding: '0 0 8px',
+                            color: selectedOption === 'conversation' ? 'white' : '#d2c1b3',
+                        }}
+                        onClick={() => setSelectedOption('conversation')}
+                    >
                         Do rozmowy
                     </Typography>
-
                 </Box>
-
             </Box>
         </Container>
     );
