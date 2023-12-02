6e7c71b7a6fd583b4118a4476b7a9ff6cad4c53b
KarolDawidG <karoldawidg@gmail.com>
2023-12-02 20:49:00 +0100

    validation.js - deleting comments

--- src/components/utils/validation.ts
+++ src/components/utils/validation.ts
@@ -5,9 +5,7 @@ import axios from 'axios';
     export const validatePortfolioUrls = (portfolioUrls: string) => {
     let urls = portfolioUrls.split('\n')
     const pattern = new RegExp('^(https?:\/\/)?([\\w-]+\\.)*[\\w-]+\\.[\\w-]+(\/[-\\w .\/?%&=]*)?$'); 
-     //poprawiona wersja - weryfikowalem na https://regexr.com/
-    //const pattern = new RegExp('https?://([\\da-z.-]+)\\.([a-z.]{2,6})[/\\w.-]*');
-    return (portfolioUrls ==="" || urls.every(x=>pattern.test(x))); // dodana opcja - portfolio może być puste, dla projektu zaliczeniowe obsluguje to formularz ustawiony jako required.
+    return (portfolioUrls ==="" || urls.every(x=>pattern.test(x)));
     };
 
     // walidacja email
@@ -18,8 +16,7 @@ import axios from 'axios';
 
     // walidacja phone-number
     export const validatePhone = (phone: string) => {
-        //const pattern = /^[0-9]{3,45}$/;      //dopuszcza liczbe ktora ma 45 cyfr
-        const pattern = /^[5,6,7,8]\d{8}$/;     //pierwsza cyfra musi zawierac sie w przedziale od 5 do 8, kolejne - lacznie 8, od 0 do 9
+        const pattern = /^[5,6,7,8]\d{8}$/;
         return (phone === "" || pattern.test(phone));
     };
 
@@ -41,7 +38,6 @@ import axios from 'axios';
     };
 
     // walidacja github user 
-    // wstepnie uproscilem te funkcje
     export const validateGithub = async (github: string) => {
         try {
             const response = await axios.get(`https://api.github.com/users/${github}`);
@@ -54,23 +50,3 @@ import axios from 'axios';
             console.error('Błąd logowania', error);
         }
     }
-
-    // export const validateGithub = async (github: string) => {
-    //     try {
-    //         const response = await axios.get(`https://api.github.com/users/${github}`, {
-    //             validateStatus: (status: number) => {
-    //                 return (status >= 200 && status < 300) || status == 404
-    //             }
-    //         });
-
-    //         if (response.status === 200) {
-    //             console.log("Status 200");
-    //         }
-    //         else {
-    //             notify("Nie znaleziono użytkownika.");
-    //         }
-
-    //     } catch (error) {
-    //         console.error('Błąd logowania', error);
-    //     }
-    // }
